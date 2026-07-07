a31edbf95476d260be055f7905d4aa247f8c4609
OlaReda <ola.reda.900@gmail.com>
2026-07-07 23:57:19 +0300

    Add logic of login and register page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-async function getData() {
-    let response = await fetch("https://api.escuelajs.co/api/v1/products")
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
+// async function getData() {
+//     let response = await fetch("https://api.escuelajs.co/api/v1/products")
+//     console.log(response)
+//     let data = await response.json()
+//     console.log(data)
+// }
+
+// getData()
+const email = document.qu
+
+function register() {
+    
 }
 
-getData()
+function login() {
+
+}
